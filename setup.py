c16dda309cfc5e8f77e9b5b7297229d5962a48de
Joel Eager <git@joeleager.me>
2026-08-27 13:15:26 -0400

    Exclude tests from future releases

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JoelEager/terminal-mines",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
         "click>=7.0",
     ],
